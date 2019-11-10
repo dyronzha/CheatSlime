57166b3f671aea2232f4a2b1c996bd85a7e2b1c0
Scream <scream870102@gmail.com>
2019-11-10 09:31:16 +0800

    M

--- CheatSlime/Assets/Script/GameManager.cs
+++ CheatSlime/Assets/Script/GameManager.cs
@@ -1,29 +1,30 @@
 using Eccentric.Utils;
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class GameManager : TSingletonMonoBehavior<GameManager> {
 
+    public int winnerID { get; set; }
 
-
-    string[] playerInput = new string[4];
-    public string[] PlayerInput {
+    string [ ] playerInput = new string [4];
+    public string [ ] PlayerInput {
         set { playerInput = value; }
         get { return playerInput; }
     }
 
-    string[] playerName = new string[4];
-    public string[] PlayerName {
-        set {playerName = value;}
-        get {return playerName;}
+    string [ ] playerName = new string [4];
+    public string [ ] PlayerName {
+        set { playerName = value; }
+        get { return playerName; }
     }
 
-    protected override void Awake()
-    {
-        base.Awake();
+    protected override void Awake ( ) {
+        base.Awake ( );
     }
 
-    public void GameEnd(int winner){
-        
+    public void GameEnd (int winner) {
+        winnerID = winnerID;
+        SceneManager.LoadScene (4);
     }
 
 }
---
+++ CheatSlime/Assets/Script/ResultManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+public class ResultManager : MonoBehaviour {
+    [SerializeField] List<Sprite> winnerSpr = new List<Sprite> ( );
+    [SerializeField] Image winnerImg = null;
+    [SerializeField] Text winnerText = null;
+    // Start is called before the first frame update
+    void Start ( ) {
+        winnerImg.sprite = winnerSpr [GameManager.Instance.winnerID];
+        winnerText.text = "Player" + GameManager.Instance.winnerID;
+    }
+}
